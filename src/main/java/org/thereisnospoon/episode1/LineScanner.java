cbe69cbf3f2d2466424d6f821314ce7a95974aca
Francois bornancin <francois.bornancin@skema.edu>
2024-10-07 20:05:37 +0200

    finalize stuff

--- src/main/java/org/thereisnospoon/episode1/LineScanner.java
+++ src/main/java/org/thereisnospoon/episode1/LineScanner.java
@@ -5,13 +5,9 @@ public class LineScanner {
     public String getRightNodeAsString(String element, int index, int heightIndex){
         String[] elementsToScan = element.substring(index).split("");
 
-        System.out.println("");
-
         for(int a = 1 ; a < elementsToScan.length ; a++){
             if(elementsToScan[a].equals("0")) return "" + (index + a) + " " + heightIndex;
         }
-
-        System.out.println("");
 
         return "-1 -1";
 
